d31ade369d4c40ed3bfa373c27359b8c159c3e4b
cmdrgh <1059675576@qq.com>
2019-02-21 16:44:03 +0800

    admin_user

--- C_of_C/src/admin/AdminUser.vue
+++ C_of_C/src/admin/AdminUser.vue
@@ -224,7 +224,7 @@ export default {
     handleDelete(index, row) {
       console.log(index, row);
 
-      this.$confirm("此操作将永久删除该用户, 是否继续?", "提示", {
+      this.$confirm("此操作将永久删除该用户 "+ row.userInfoName +", 是否继续?", "提示", {
         confirmButtonText: "确定",
         cancelButtonText: "取消",
         type: "warning"
